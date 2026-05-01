a0f530d77ce7b0ed130d7079bfb0781a80315665
fagbrevsomvoksen-alt <fagbrevsomvoksen@gmail.com>
2026-05-01 13:45:40 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
    Google Fonts cached at runtime.
    ========================================= */
 
-const CACHE_VERSION = 'japan-journey-v13';
+const CACHE_VERSION = 'japan-journey-v14';
 const APP_SHELL = [
   './',
   './index.html',
